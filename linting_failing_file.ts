3449be655c6ee6c3916d7beefabc4cdf2f1fa3ce
Dev Thakkar <90659457+DevT9@users.noreply.github.com>
2023-09-24 14:19:10 +0000

    push

---
+++ linting_failing_file.ts
@@ -0,0 +1,16 @@
+const myFunction = (a: number, b: number): void => {
+    if(a === 0) {
+        console.log("a is zero");
+    }
+    else if(b === 0) {
+        console.log("b is zero");
+    }
+    else {
+        console.log("a + b = " + (a + b));
+    }
+}
+
+myFunction(1, 2);
+myFunction(0, 3);
+myFunction(4, 0);
+myFunction(0, 0);
